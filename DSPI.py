9bcd37b31b34c87fdd2d284b25e596b05ceddfb4
Etienne van de Bijl <Etienne van de Bijl>
2023-07-13 17:18:08 +0200

    Distinguish code

---
+++ DSPI.py
@@ -0,0 +1,74 @@
+import DutchDraw as DutchDraw
+import math
+import numpy as np
+
+measure_dictionary = DutchDraw.measure_dictionary
+
+def DSPI(y_true, measure, alpha, thetaopts, rho = 0, beta = 1):
+    
+    P = np.int64(sum(y_true))
+    M = np.int64(len(y_true))
+    N = np.int64(M - P)
+    
+    t = thetaopts[0]
+    TP = alpha * P * (1 - rho - t) + P * t 
+    TN = alpha * N * (t - rho) + N * (1 - t) 
+    FN = alpha * P * (rho - 1 + t) + P * (1 - t)
+    FP = alpha * N * (rho - t) + N * t
+
+    if measure in measure_dictionary['PPV']:
+        return TP / (TP + FP)
+
+    if measure in measure_dictionary['NPV']:
+        return TN / (TN + FN)
+
+    if measure in measure_dictionary['FDR']:
+        return FP / (TP + FP)
+
+    if measure in measure_dictionary['FOR']:
+        return FN / (TN + FN)
+
+    if measure in measure_dictionary['ACC']:
+        return (TP + TN) / M
+
+    if measure in measure_dictionary['BACC']:
+        TPR = TP / P
+        TNR = TN / N
+        return (TPR + TNR) / 2
+
+    if measure in measure_dictionary['FBETA']:
+        beta_squared = beta ** 2
+        return (1 + beta_squared) * TP / (((1 + beta_squared) * TP) + (beta_squared * FN) + FP)
+
+    if measure in measure_dictionary['MCC']:
+        return (TP * TN - FP * FN)/(math.sqrt((TP + FP) * (TN + FN) * P * N))
+
+    if measure in measure_dictionary['J']:
+        TPR = TP / P
+        TNR = TN / N
+        return TPR + TNR - 1
+
+    if measure in measure_dictionary['MK']:
+        PPV = TP / (TP + FP)
+        NPV = TN / (TN + FN)
+        return PPV + NPV - 1
+
+    if measure in measure_dictionary['KAPPA']:
+        P_o = (TP + TN) / M
+        P_yes = ((TP + FP) / M) * (P / M)
+        P_no = ((TN + FN) / M) * (N / M)
+        P_e = P_yes + P_no
+        return (P_o - P_e) / (1 - P_e)
+
+    if measure in measure_dictionary['FM']:
+        TPR = TP / P
+        PPV = TP / (TP + FP)
+        return math.sqrt(TPR * PPV)
+
+    if measure in measure_dictionary['G2']:
+        TPR = TP / P
+        TNR = TN / N
+        return np.sqrt(TPR * TNR)
+
+    if measure in measure_dictionary['TS']:
+        return TP / (TP + FN + FP)
